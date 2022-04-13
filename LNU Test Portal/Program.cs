6df8a926156b9aa28e1d42a9ffab7daa3f5344ce
SviatoslavKondera <svatoslavkondera@gmail.com>
2022-04-13 23:21:30 +0300

    new

--- LNU Test Portal/Program.cs
+++ LNU Test Portal/Program.cs
@@ -6,19 +6,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace LNU_Test_Portal
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                // CreateHostBuilder(args).Build().Run();
+                Console.ReadKey(true);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
         }
+        //CreateHostBuilder(args).Build().Run();
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() // <-- Add this line
                 .ConfigureWebHostDefaults(webBuilder =>
+
                 {
                     webBuilder.UseStartup<Startup>();
                 });
